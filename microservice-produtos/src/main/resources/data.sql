d0f1fc5125ec51e322c702f6b5f6400f88644d16
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2025-08-03 20:12:19 -0300

    Atualiza .gitignore para Java + Frontend

--- microservice-produtos/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO produtos (nome, descricao, preco) VALUES ('Notebook', 'Notebook Dell Inspiron', 3500.00);
-INSERT INTO produtos (nome, descricao, preco) VALUES ('Mouse Gamer', 'Mouse com 6 botões programáveis', 120.00);
-INSERT INTO produtos (nome, descricao, preco) VALUES ('Teclado Mecânico', 'Teclado com iluminação RGB', 250.00);
